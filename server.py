c070099b115f07efe5beb1f715a3e7755218ffc4
joaoh9 <joaoh9.costa@gmail.com>
2018-12-09 15:50:41 +0000

    venv

---
+++ server.py
@@ -0,0 +1,43 @@
+import json
+import sys
+from flask import Flask
+app = Flask('TP3')
+
+def main():
+    #port = int(sys.argv[1])
+    Netfile = open(sys.argv[1], "r")
+    #Ixfile = open(sys.argv[3], "r")
+    #Netixlanfile = open(sys.argv[4], "r")
+
+    Netfile_data = Netfile.read()
+    Netfile_json = json.loads(Netfile_data)
+    print (Netfile_json.keys())
+
+    # print (Netfile_json.values())
+    print (Netfile_json["meta"])
+
+    
+    @app.route('/')
+    def hello():
+        return str(Netfile_json["data"])
+        # 1) campo id do arquivo net.json
+        # 2) campo name do arquivo net.json
+        # numero IPX associado a rede - deve ser gerado pelo cliente
+
+
+    @app.route("/api/ix") # IXPs por rede
+    def all_ipx():
+        return 'Hello'
+    
+    @app.route("/api/ixnets/<ix_id>") # Redes por IXP
+    def id_redes_ixp():
+       return True
+
+    @app.route("/api/netname/<net_id>") # Nome da rede
+    def rede_name():
+       return True
+    
+    app.run(use_reloader=True)
+
+if __name__ == "__main__":
+    main()
